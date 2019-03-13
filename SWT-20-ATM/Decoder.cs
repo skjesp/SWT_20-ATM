9cf7cabed5efef3c3580ecc423b3134cea7f0168
lassesos <lazze95@live.dk>
2019-03-13 11:33:38 +0100

    Added Plane and Decoder
    added plane, idecoder, decoder

---
+++ SWT-20-ATM/Decoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SWT_20_ATM
+{
+    public class Decoder : IDecoder
+    {
+        private List<Plane> decoderList;
+        public void Decode(List<string> newData)
+        {
+            decoderList.Clear();
+            foreach (var data in newData)
+            {
+                string[] result = data.Split(';');
+
+                DateTime tempDateTime = DateTime.ParseExact(result[4], "yyyyMMddHHmmssfff", null);
+
+                Plane tempPlane = new Plane(result[0],int.Parse(result[1]), int.Parse(result[2]), int.Parse(result[3]), tempDateTime);
+                
+                decoderList.Add(tempPlane);
+
+            }
+        }
+    }
+}
---
+++ SWT-20-ATM/IDecoder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SWT_20_ATM
+{
+    public interface IDecoder
+    {
+        void Decode(List<string> newData);
+    }
+}
