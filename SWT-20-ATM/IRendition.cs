f451df2dd7ec5e8a9f1b4c9f3419877b49d4849a
rasmus godske <rasmusgodske@gmail.com>
2019-03-19 14:45:24 +0100

    Actually deleted non needed files and added new classes

--- SWT-20-ATM/IRendition.cs
+++ SWT-20-ATM/IRendition.cs
@@ -2,12 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SWT_20_ATM
 {
-    interface IAirspaceRules
+    public interface IRendition
     {
-        void SetDistanceRule(int distance, int height);
     }
 }
