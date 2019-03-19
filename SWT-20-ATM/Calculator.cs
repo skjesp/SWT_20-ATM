fa2a7b7b003e3ab1d20059f4ee2fe4e9085cec47
rasmus godske <rasmusgodske@gmail.com>
2019-03-19 15:55:45 +0100

    Added calculator.GetDistance()

--- SWT-20-ATM/Calculator.cs
+++ SWT-20-ATM/Calculator.cs
@@ -18,20 +18,29 @@ namespace SWT_20_ATM
             return degrees;
         }
 
-        public double GetSpeed(int x1, int y1, DateTime date1, int x2, int y2, DateTime date2)
+        public double GetDistance(int x1, int y1, int x2, int y2)
         {
-            // Get seconds difference between dates
-            double secondDif = date2.Subtract(date1).TotalSeconds;
             double dx = x1 - x2;
             double dy = y1 - y2;
 
             // Calculate distance between coordinates (Meters)
-            double distanceDif = Math.Sqrt( Math.Pow(dx, 2) + Math.Pow(dy,2) );
+            return Math.Sqrt(Math.Pow(dx, 2) + Math.Pow(dy, 2));
+        }
+
+        public double GetSpeed(int x1, int y1, DateTime date1, int x2, int y2, DateTime date2)
+        {
+            // Get seconds difference between dates
+            double secondDif = date2.Subtract(date1).TotalSeconds;
+
+            // Calculate distance between coordinates (Meters)
+            double distance = this.GetDistance(x1, y1, x2, y2);
 
             // Calculate traveling speed unit (Meters / second)
-            double speed = Math.Abs(distanceDif / secondDif);
+            double speed = Math.Abs(distance / secondDif);
 
             return speed;
         }
+
+        
     }
 }
