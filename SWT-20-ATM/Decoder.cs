d286f44038ef873ef511fb994e572bf88b6cca40
rasmus godske <rasmusgodske@gmail.com>
2019-03-21 15:45:50 +0100

    Decoder now got it own event that will be called when new planeList is given. Decoder also updates new planes speed and direction(not tested)

--- SWT-20-ATM/Decoder.cs
+++ SWT-20-ATM/Decoder.cs
@@ -5,16 +5,20 @@ namespace SWT_20_ATM
 {
     public class Decoder : IDecoder
     {
-        public List<Plane> decoderList;
+        public delegate void NewPlaneListEvent(List<Plane> planeList);
+        public event NewPlaneListEvent NewPlanesEvent;
+
+        public List<Plane> OldPlaneList;
 
         public Decoder()
         {
-            decoderList = new List<Plane>();
+            OldPlaneList = new List<Plane>();
         }
 
         public void Decode(List<string> newData)
         {
-            decoderList.Clear();
+            List<Plane> newPlaneList = new List<Plane>();
+            
             foreach (var data in newData)
             {
                 string[] result = data.Split(';');
@@ -22,12 +26,48 @@ namespace SWT_20_ATM
                 DateTime tempDateTime = DateTime.ParseExact(result[4], "yyyyMMddHHmmssfff", null);
 
                 Plane tempPlane = new Plane(result[0],int.Parse(result[1]), int.Parse(result[2]), int.Parse(result[3]), tempDateTime);
-                
-                decoderList.Add(tempPlane);
 
-                // insert ATM newDate call
-
+                newPlaneList.Add(tempPlane);
             }
+
+            // Update the speed and direction of the new planes found
+            foreach (var oldPlane in OldPlaneList)
+            {
+                foreach (var newPlane in newPlaneList)
+                {
+                    if (newPlane.Tag == oldPlane.Tag)
+                    {
+                        newPlane.Update(oldPlane);      // Update plane found in record
+                        break;                          // No reason to loop rest of planes
+                    }
+                }
+            }
+
+            
+            // Update oldPlaneList with new planes
+            OldPlaneList = newPlaneList;
+
+            // Get all planes that got all properties
+            List<Plane> completePlaneList = GetCompletePlanes(newPlaneList);
+
+            // Call event
+            NewPlanesEvent?.Invoke(completePlaneList);
+        }
+
+        public List<Plane> GetCompletePlanes(List<Plane> planeList)
+        {
+            List<Plane> completePlaneList = new List<Plane>();  // List that will contain all complete planes
+
+            foreach (var plane in planeList)
+            {
+                if (double.IsNaN(plane.speed)) continue;        // Do not add plane if speed is NaN
+                if (double.IsNaN(plane.direction)) continue;    // Do not add plane if direction is NaN
+
+                completePlaneList.Add(plane);                   // Add plane to list that will be returned
+            }
+
+            return completePlaneList;
         }
     }
+
 }
