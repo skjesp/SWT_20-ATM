a7a0620800e6aee10d98187d4295e47a05e3c28b
rasmus godske <rasmusgodske@gmail.com>
2019-03-21 16:17:20 +0100

    Program.cs now creates: Decoder, Airspace, Shapes, ATM. Still not finished

--- SWT-20-ATM/Program.cs
+++ SWT-20-ATM/Program.cs
@@ -20,9 +20,23 @@ namespace SWT_20_ATM
             // Dependency injection with the real TDR
             var system = new TransponderReceiverClient(receiver);
 
+            // Decoder that decodes strings into plane objects
             Decoder myDecoder = new Decoder();
 
+            // Make decoder listen to system events
             system.NewPlanesEvent += myDecoder.Decode;
+
+            // Create used airspace
+            Airspace airspace = new Airspace();
+
+            // Add area to airspace
+            airspace.AddShape(new Cuboid(0,0, 500,80000,80000,20000));
+            
+            // Air Traffic Monitor
+            ATM atm = new ATM(airspace, 300, 5000);
+
+            myDecoder.NewPlanesEvent += atm.updatePlaneList;
+            
 
             // Let the real TDR execute in the background
             while (true)
