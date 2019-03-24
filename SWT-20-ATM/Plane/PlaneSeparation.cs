4be5d4df70ad887ab603dd368fcc67b444f4410d
rasmus godske <rasmusgodske@gmail.com>
2019-03-24 22:26:21 +0100

    PlaneSeparation now uses the Plane Interface instead of Plane Class. PlaneSeparation has been refactored.

--- SWT-20-ATM/Plane/PlaneSeparation.cs
+++ SWT-20-ATM/Plane/PlaneSeparation.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace SWT_20_ATM
 {
@@ -9,43 +7,53 @@ namespace SWT_20_ATM
     {
         private int _minVerticalDistance = 0;
         private int _minHorizontalDistance = 0;
-        
 
-        public PlaneSeparation(int h, int v)
+
+        public PlaneSeparation( int h, int v )
         {
-            SetDistance(h,v);
+            SetDistance( h, v );
         }
 
-        public void SetDistance(int h, int v)
+        public void SetDistance( int h, int v )
         {
             _minHorizontalDistance = h;
             _minVerticalDistance = v;
         }
 
-        public List<List<Plane>> CheckPlanes(List<Plane> planeList)
+        public List<List<IPlane>> CheckPlanes( List<IPlane> planeList )
         {
-            List<List<Plane>> violatingPlanes = new List<List<Plane>>();
+            List<List<IPlane>> violatingPlanes = new List<List<IPlane>>();
 
-            foreach (var Plane in planeList)
+            foreach ( var Plane in planeList )
             {
-                foreach (var comparePlane in planeList)
+                foreach ( var comparePlane in planeList )
                 {
-                    if (Plane.Tag == comparePlane.Tag) continue;    // Do nothing if plane being compared is the same
+                    if ( Plane.Tag == comparePlane.Tag )
+                    {
+                        continue;    // Do nothing if plane being compared is the same
+                    }
 
-                    double horizontalDifference = Calculator.GetDistance(Plane.XCoordinate, Plane.YCoordinate, comparePlane.XCoordinate, comparePlane.YCoordinate);
-                    int verticalDifference = Math.Abs(Plane.Altitude - comparePlane.Altitude);
+                    double horizontalDifference = Calculator.GetDistance( Plane.XCoordinate, Plane.YCoordinate, comparePlane.XCoordinate, comparePlane.YCoordinate );
+                    int verticalDifference = Math.Abs( Plane.Altitude - comparePlane.Altitude );
 
-                    if (horizontalDifference >= _minHorizontalDistance) continue;   // Do nothing if horizontal difference is large enough
-                    if (verticalDifference   >= _minVerticalDistance)   continue;   // Do nothing if vertical   difference is large enough
+                    if ( horizontalDifference >= _minHorizontalDistance )
+                    {
+                        continue;   // Do nothing if horizontal difference is large enough
+                    }
+
+                    if ( verticalDifference >= _minVerticalDistance )
+                    {
+                        continue;   // Do nothing if vertical   difference is large enough
+                    }
 
                     // Create plane pair
-                    List<Plane> violatingPlanePair = new List<Plane>
+                    List<IPlane> violatingPlanePair = new List<IPlane>
                     {
                         Plane,
                         comparePlane
                     };
 
-                    violatingPlanes.Add(violatingPlanePair);
+                    violatingPlanes.Add( violatingPlanePair );
 
                 }
             }
