70788794a861f0c33b74d75dd1421e8c3cd3d212
rasmus godske <rasmusgodske@gmail.com>
2019-03-24 21:04:13 +0100

    Made a interface for Plane, used for unittesting. Updated Plane to use both private variables aswell having a public interface.

---
+++ SWT-20-ATM/Plane/IPlane.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SWT_20_ATM
+{
+    public interface IPlane
+    {
+        bool Update( IPlane oldPlane );
+        string Tag { get; }
+        int XCoordinate { get; }
+        int YCoordinate { get; }
+        int Altitude { get; }
+        double Speed { get; }
+        double Direction { get; }
+        DateTime LastUpdate { get; }
+    }
+}
