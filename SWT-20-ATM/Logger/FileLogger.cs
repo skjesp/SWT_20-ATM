0a91dbcfb30f293bce95b2aa3955e74d22a84121
Mads Bjerg <au566699@uni.au.dk>
2019-03-24 22:17:24 +0100

    Added code for filelogger

--- SWT-20-ATM/Logger/FileLogger.cs
+++ SWT-20-ATM/Logger/FileLogger.cs
@@ -1,12 +1,30 @@
-﻿//using System;
+﻿using System;
+using System.IO;
+namespace SWT_20_ATM
+{
+    public class FileLogger : ILogger
+    {
+        public FileLogger(string filePath)
+        {
+            if (filePath == null)
+            {
+                _filePath = "../SepLog.txt";
+                File.Open(_filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            }
+            else
+            {
+                _filePath = filePath;
+                File.Open(_filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            }
 
-//namespace SWT_20_ATM
-//{
-//    public class FileLogger : ILogger
-//    {
-//        public void AddToLog(Separation SepToLog)
-//        {
-//            SepToLog.Write();
-//        }
-//    }
-//}
+        }
+        public void AddToLog(PlaneSeparation SepToLog)
+        {
+            StringWriter sw = new StringWriter();
+            sw.WriteLine(_filePath, SepToLog);
+            sw.Flush();
+        }
+
+        string _filePath;
+    }
+}
--- SWT-20-ATM/Logger/ILogger.cs
+++ SWT-20-ATM/Logger/ILogger.cs
@@ -1,9 +1,10 @@
-﻿//using System;
+﻿using System;
 
-//namespace SWT_20_ATM
-//{
-//    public interface ILogger
-//    {
-//        void AddToLog(Separation SepToLog);
-//    }
-//}
+namespace SWT_20_ATM
+{
+    public interface ILogger
+    {
+        void AddToLog(PlaneSeparation SepToLog);
+        
+    }
+}
